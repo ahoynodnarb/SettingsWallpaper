12cff9a19ad67cb9a31016c0081de7ac52e4fd31
Corban Amouzou <c0dineDev@gmail.com>
2021-03-20 18:28:30 -0700

    Added cell translucency for all PSTableCells

--- SettingsWallpaper.h
+++ SettingsWallpaper.h
@@ -1,6 +1,13 @@
+/*
+ *   Copyright (c) 2021 c0dine
+ *   All rights reserved.
+ *   Feel free to contribute!
+ */
 #import <UIKit/UIKit.h>
 BOOL enabled = YES;
 BOOL blur = YES;
+BOOL alpha = YES;
 int blurType = 0;
 float intensity = 1.0f;
 int wallpaperMode = 0;
+float cellAlpha = 1.0f;
